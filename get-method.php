7396c0c365ec7876afdab5646df8682b7663a403
Kanch-prog <121807277+Kanch-prog@users.noreply.github.com>
2023-01-03 05:13:31 +0530

    Add files via upload

---
+++ get-method.php
@@ -0,0 +1,44 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+
+if(isset($_POST['name']) && isset($_POST['email'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    require_once "PHPMailer/PHPMailer.php";
+    require_once "PHPMailer/SMTP.php";
+    require_once "PHPMailer/Exception.php";
+
+    $mail = new PHPMailer();
+
+    //smtp settings
+    $phpmailer = new PHPMailer();
+    $phpmailer->isSMTP();
+    $phpmailer->Host = 'smtp.mailtrap.io';
+    $phpmailer->SMTPAuth = true;
+    $phpmailer->Port = 2525;
+    $phpmailer->Username = '5970d3d6002f66';
+    $phpmailer->Password = '490535b986d583';
+    $phpmailer->SMTPSecure = "tsl";
+
+    //email settings
+    $mail->isHTML(true);
+    $mail->setFrom($email, 'Mailtrap Website');
+    $mail->addAddress("kanchana.karunarathna15@gmail.com");
+    $mail->Subject = 'New message from your website';
+    
+    if($mail->send()){
+        $status = "success";
+        $response = "Email is sent!";
+    }
+    else
+    {
+        $status = "failed";
+        $response = "Something is wrong: <br>" . $mail->ErrorInfo;
+    }
+
+    exit(json_encode(array("status" => $status, "response" => $response)));
+}
+
+?>
